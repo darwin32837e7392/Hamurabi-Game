b4802f0ff7548c1426bf1b047b0b47c6b4580cd2
cruci fix <crucifix@crucifixs-MacBook-Pro.local>
2021-11-07 14:26:29 +0100

    worked on acre logic

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { evaluate } from 'mathjs'
 
 class Game extends React.Component {
     constructor(props) {
@@ -11,15 +12,48 @@ class Game extends React.Component {
             acres: 1000,
             harvested: 3,
             rats: 200,
-            bushelsTotal: 2800,
+            bushelsInStore: 2800,
             landCost: 17,
             acresMarket: 0,
             bushelsFed: 0,
-            seedsPlanted: 0
+            seedsPlanted: 0,
+            bushelsRemaining: 2800,
+            acresRemaining: 1000
         };
         this.gameLoop = this.gameLoop.bind(this);
         this.calculateScore = this.calculateScore.bind(this);
         this.getRandom = this.getRandom.bind(this);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const { acresMarket, bushelsFed, seedsPlanted, acresRemaining, landCost, bushelsRemaining, bushelsInStore } = this.state;
+        if (acresMarket !== prevState.acresMarket && acresMarket !== 0 && acresMarket !== '') {
+            // check if player is selling more land than they own or buying more than they can afford
+            if (0 <= evaluate(`${acresMarket} + ${acresRemaining}`) && 0 <= this.calculateBushelsRemaining(acresMarket, bushelsFed, seedsPlanted)) {
+                this.setState({
+                    bushelsRemaining: this.calculateBushelsRemaining(acresMarket, bushelsFed, seedsPlanted)
+                });
+            } else {
+                // Set acres to 0 if the player breaks the rules
+                this.setState({
+                    acresMarket: 0,
+                    bushelsRemaining: this.calculateBushelsRemaining(0, bushelsFed, seedsPlanted)
+                })
+            }
+        } else if (bushelsFed !== prevProps.bushelsFed && bushelsFed !== '' && acresMarket !== '') {
+            if(0 <= this.calculateBushelsRemaining(acresMarket, bushelsFed, seedsPlanted)) {
+                
+            } else {
+
+            }
+        } else if (seedsPlanted !== prevProps.seedsPlanted && seedsPlanted !== '' && bushelsFed !== '' && acresMarket !== '') {
+
+        }
+    }
+
+    calculateBushelsRemaining(acresMarket, bushelsFed, seedsPlanted) {
+        const { landCost, bushelsInStore } = this.state;
+        return evaluate(`${bushelsInStore} - (${acresMarket} * ${landCost}) - ${bushelsFed} - ${seedsPlanted}`)
     }
 
     gameLoop() {
@@ -59,8 +93,8 @@ class Game extends React.Component {
     }
 
     render() {
-        const { year, starved, peopleImmigrated, population, acres, harvested, rats, bushelsTotal, landCost, acresMarket,
-        bushelsFed, seedsPlanted } = this.state;
+        const { year, starved, peopleImmigrated, population, acres, harvested, rats, bushelsInStore, landCost, acresMarket,
+        bushelsFed, seedsPlanted, bushelsRemaining, acresRemaining } = this.state;
         return (
             <div className='game'>
                 <h1>Hammurabi I beg to report to you,</h1>
@@ -70,9 +104,13 @@ class Game extends React.Component {
                 <h2>The city now owns {acres} acres.</h2>
                 <h2>You harvested {harvested} bushels per acre.</h2>
                 <h2>Rats ate {rats} bushels.</h2>
-                <h2>You now have {bushelsTotal} bushels in store.</h2>
+                <h2>You now have {bushelsInStore} bushels in store.</h2>
                 <h2>Land is trading at {landCost} bushels per acre.</h2>
-                <input type='number' value={acres} onChange={event => this.setState({ acres: event.target.value})}/>
+                <h2>{bushelsRemaining} bushels remaining.</h2>
+                <h2>{acresRemaining} acres remaining.</h2>
+                <input type='number' value={acresMarket} onChange={event => this.setState({ acresMarket: event.target.value})}/>
+                <input type='number' value={bushelsFed} onChange={event => this.setState({ bushelsFed: event.target.value})}/>
+                <input type='number' value={seedsPlanted} onChange={event => this.setState({ seedsPlanted: event.target.value})}/>
                 <button onClick={() => this.gameLoop()}>Make It So!</button>
             </div>
         )
